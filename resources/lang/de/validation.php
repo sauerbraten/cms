a9b732668bd33a981497223aae41f1f6f10cf401
Wiebke Vogel <wiebke.vogel@spiegel.de>
2021-03-23 10:59:46 +0100

    Added translation for new validation

--- resources/lang/de/validation.php
+++ resources/lang/de/validation.php
@@ -86,6 +86,7 @@ return [
     'url' => 'Das Format ist ungültig.',
     'unique_entry_value' => 'Dieser Wert wurde bereits vergeben.',
     'unique_user_value' => 'Dieser Wert wurde bereits vergeben.',
+    'unique_value' => 'Dieser Wert wurde bereits vergeben.',
     'duplicate_field_handle' => 'Feld mit dem Handle :handle kann nicht mehr als einmal verwendet werden.',
     'one_site_without_origin' => 'Mindestens eine Website darf keine Quelle enthalten.',
     'origin_cannot_be_disabled' => 'Kann keine deaktivierte Quelle auswählen.',
